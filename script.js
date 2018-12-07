a6c1878cb2f99db7bc48da4f4cd633392d33640b
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2018-12-07 00:53:36 -0500

    Update script to follow standard for loading jQuery

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
+/* Using a self-executing anonymous function - (function(){})(); - so that all variables and functions defined within 
+aren’t available to the outside world. */
 
-$(document).ready(function() {
-    
-    const SHOP = window.Shopify.shop
-    const API_BASE = `https://${SHOP}/apps/mindset-api-v1`
+(function(){
 
-    $.get(`${API_BASE}/test`)
+    var SHOP = window.Shopify.shop
+    var API_BASE = `https://${SHOP}/apps/mindset-api-v1`
+  
+    /* Load Script function we may need to load jQuery from the Google's CDN */
+    /* That code is world-reknown. */
+    /* One source: http://snipplr.com/view/18756/loadscript/ */
 
-});
+    var loadScript = function(url, callback){
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+
+        // If the browser is Internet Explorer.
+        if (script.readyState){ 
+            script.onreadystatechange = function(){
+                if (script.readyState == "loaded" || script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        // For any other browser.
+        } else {
+            script.onload = function(){
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    };
+
+    var buildCardTemplate = function (productData) {
+        // TODO: implement
+    };
+
+    var buildRecommendationSectionTemplate = function (recommendationsResponse) {
+        // TODO: implement
+    };
+
+    /* This is my app's JavaScript */
+    var initialize = function($){
+        $.get(`${API_BASE}/recommendations/similar`, function(data) {
+            var html = buildRecommendationSectionTemplate(data);
+            $('#some-id').append(html);
+        });
+    };
+
+    /* If jQuery has not yet been loaded or if it has but it's too old for our needs,
+    we will load jQuery from the Google CDN, and when it's fully loaded, we will run
+    our app's JavaScript. Set your own limits here, the sample's code below uses 1.7
+    as the minimum version we are ready to use, and if the jQuery is older, we load 1.9. */
+    if ((typeof jQuery === 'undefined') || (parseInt(jQuery.fn.jquery) === 1 && parseFloat(jQuery.fn.jquery.replace(/^1\./,'')) < 7.1)) {
+        loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function(){
+            jQuery191 = jQuery.noConflict(true);
+            initialize(jQuery191);
+        });
+    } else {
+        initialize(jQuery);
+    }
+
+})();
